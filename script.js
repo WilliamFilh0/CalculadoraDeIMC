a48e994afda46a00a37c0eaef6be5b3e1bffa9fa
WilliamFilh0 <williamfilho8908@gmail.com>
2023-08-08 08:39:30 -0300

    Estruturando os dados de modal

--- script.js
+++ script.js
@@ -3,10 +3,23 @@ const form = document.querySelector('form')
 const inputWeight = document.querySelector('#weight')
 const inputHeight = document.querySelector('#height')
 
-const modalWrapper = document.querySelector('.modal-wrapper')
+/*const modalWrapper = document.querySelector('.modal-wrapper')
 const modalMessage = document.querySelector('.modal .title span')
-const modalBtnClose = document.querySelector('.modal button.close')
+const modalBtnClose = document.querySelector('.modal button.close')*/
 
+const Modal = {
+
+  wrapper: document.querySelector('.modal-wrapper'),
+  message: document.querySelector('.modal .title span'),
+  buttonClose: document.querySelector('.modal button.close'),
+
+  open() {
+    Modal.wrapper.classList.add('open')
+  },
+  close() {
+    Modal.wrapper.classList.remove('open')
+  }
+}
 
 form.onsubmit = event => {
   event.preventDefault()
@@ -14,16 +27,18 @@ form.onsubmit = event => {
   const weight = inputWeight.value
   const height = inputHeight.value
 
-  const result = IMC(weight,height)
+  const result = IMC(weight, height)
   const message = `Seu IMC é de ${result}`
 
-  modalMessage.innerText = message
-  modalWrapper.classList.add('open')
+  Modal.message.innerText = message
+  
+  Modal.open()
 
 }
 
-modalBtnClose.onclick = () => {
-  modalWrapper.classList.remove('open')
+Modal.buttonClose.onclick = () => {
+  
+  Modal.close()
 }
 
 
